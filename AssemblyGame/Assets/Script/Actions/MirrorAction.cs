d5122768068358bfd97fd0204521beceb1298af0
didaclis <36189205+didaclis@users.noreply.github.com>
2020-01-22 00:45:05 +0100

    blur on picking

--- AssemblyGame/Assets/Script/Actions/MirrorAction.cs
+++ AssemblyGame/Assets/Script/Actions/MirrorAction.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 
 public class MirrorAction : AbstractAction
 {
+    public Camera camera;
+    public GameObject current_wall;
+    Vector3 on_position;
     public override void OnAction()
     {
         GetComponent<DialogTrigger>().TriggerDialogue();
+        camera.GetComponent<blur>().BlurIn();
+        on_position = transform.position;
+        transform.position = Vector3.zero + current_wall.transform.forward;
     }
 }
