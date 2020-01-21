12d64b535d4f0cfe29eeadec0fc808ee0097d617
didaclis <36189205+didaclis@users.noreply.github.com>
2020-01-21 14:40:46 +0100

    mousePicking

--- AssemblyGame/Assets/Script/GeneralScripts/MousePicking.cs
+++ AssemblyGame/Assets/Script/GeneralScripts/MousePicking.cs
@@ -1,30 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MousePicking : MonoBehaviour
 {
-    [SerializeField] Material light_material;
-
     private Transform selected_object;
     // Update is called once per frame
     void Update()
     {
-        //if(selected_object != null)
-        //{
-        //    var selection_render = selected_object.GetComponent<Renderer>();
-        //    selection_render.material = null;
-        //    selected_object = null;
-        //}
+        if (selected_object != null)
+        {
+            var selection_render = selected_object.GetComponent<Image>();
+            selection_render.material.color = Color.white;
+            selected_object = null;
+        }
 
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        Physics.Raycast(ray, out hit);
-        var selection = hit.transform;
-        var selection_r = GetComponent<Renderer>();
-        if(selection_r != null)
+        if (Physics.Raycast(ray, out hit))
         {
-            selection_r.material = light_material;
+            var selection = hit.transform;
+            Debug.Log(selection);
+
+            if (selection != null)
+            {
+                var selection_r = selection.GetComponent<Image>();
+                if (selection_r != null)
+                {
+                    selection_r.material.color = Color.yellow;
+                }
+                selected_object = selection;
+            }
         }
     }
 }
