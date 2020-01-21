df10d21940ec29be5a281502241ee2cb580349b6
Gerard Clotet Rodriguez <36155703+GerardClotet@users.noreply.github.com>
2020-01-21 21:01:09 +0100

    Blur and darkish

---
+++ AssemblyGame/Assets/Script/GeneralScripts/blur.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blur : MonoBehaviour
+{
+    // Start is called before the first frame update
+    CameraMovement cam_m;
+    private float angle;
+
+    public GameObject darkish;
+   public GameObject blurr;
+    void Start()
+    {
+        cam_m = GetComponent<CameraMovement>();
+    }
+
+    public void BlurIn()
+    {
+        blurr.SetActive(true);
+        darkish.SetActive(true);
+    }
+    public void BlurOut()
+    {
+        blurr.SetActive(false);
+        darkish.SetActive(false);
+    }
+
+
+}
