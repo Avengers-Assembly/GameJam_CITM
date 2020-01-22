b9102f286153e28ad2d11e0ea920cd2dfb90711a
Sebi Lopez <36292214+Sebi-Lopez@users.noreply.github.com>
2020-01-22 12:23:42 +0100

    Pushed Some of the Final Events, still to be updated to unity

---
+++ AssemblyGame/Assets/Script/EventSystem/Events/Act4/E_Final_2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class E_Final_2 : EventAbstract
+{
+    public GameObject phone; 
+    public override void DoAction()
+    {
+        if (GameObject.Find("CustomEventManager").GetComponent<EventSystem>().killed)
+        {
+            phone.GetComponent<AudioSource>().Play();
+        }
+
+        else
+        {
+            // Do nothing
+        }
+    }
+}
