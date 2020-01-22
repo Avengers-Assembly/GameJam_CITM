c47183b2d18516cbf86d5944e6b356e5f863661f
Sebi Lopez <36292214+Sebi-Lopez@users.noreply.github.com>
2020-01-22 13:56:57 +0100

    Added drawer sfx

--- AssemblyGame/Assets/Script/EventSystem/Events/Act3/E_Shelf3_1.cs
+++ AssemblyGame/Assets/Script/EventSystem/Events/Act3/E_Shelf3_1.cs
@@ -7,6 +7,7 @@ public class E_Shelf3_1 : EventAbstract
     public GameObject window;
     public override void DoAction()
     {
+        actor.GetComponent<AudioSource>().Play();
         actor.GetComponent<DialogTrigger>().TriggerDialogue(1);
 
         // Active window sound
