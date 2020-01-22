0b93fe5ded0277442a7a16db0bb844394e1c8024
didaclis <36189205+didaclis@users.noreply.github.com>
2020-01-22 05:35:47 +0100

    solve dialeg sistem

--- AssemblyGame/Assets/Script/DialogScripts/DialogTrigger.cs
+++ AssemblyGame/Assets/Script/DialogScripts/DialogTrigger.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class DialogTrigger : MonoBehaviour
 {
-    public Dialogue dialogue;
+    public Dialogue[] dialogue;
+    public void TriggerDialogue(int index)
+    {
+         FindObjectOfType<DialogueManager>().StartDialogue(dialogue[index]);
+    }
     public void TriggerDialogue()
     {
-        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue[0]);
+    }
+    public void SwapDefault(int index)
+    {
+        if (dialogue[index] == null)
+            return;
+        Dialogue aux = dialogue[0];
+        dialogue[0] = dialogue[index];
+        dialogue[index] = aux;
     }
 }
