2f46e645ee7fa381683d95fcd9f35ebfefe3d690
Gerard Clotet Rodriguez <36155703+GerardClotet@users.noreply.github.com>
2020-01-22 03:08:59 +0100

    update pickable objects in the

--- AssemblyGame/Assets/Script/Actions/BasicAction.cs
+++ AssemblyGame/Assets/Script/Actions/BasicAction.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MirrorAction : AbstractAction
+public class BasicAction : AbstractAction
 {
     public Camera camera;
     public GameObject dialogueManager;
