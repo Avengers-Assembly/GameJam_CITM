6d7c7f28be5f702302b12579d47d5294e70c65c3
Gerard Clotet Rodriguez <36155703+GerardClotet@users.noreply.github.com>
2020-01-22 05:54:44 +0100

    update selectable objects

---
+++ AssemblyGame/Assets/Script/Actions/staticBasicAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class staticBasicAction : AbstractAction
+{
+    public Camera camera;
+    public GameObject dialogueManager;
+    Vector3 on_position;
+    public override void DoMyOwnAction()
+    {
+        GetComponent<DialogTrigger>().TriggerDialogue();
+        dialogueManager.GetComponent<DialogueManager>().SetObjectPicked(gameObject);
+        camera.GetComponent<blur>().BlurIn();
+  
+    }
+
+    public override void EndAction()
+    {
+        camera.GetComponent<blur>().BlurOut();
+       
+    }
+}
