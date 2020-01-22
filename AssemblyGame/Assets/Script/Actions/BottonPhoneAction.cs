aa460adb46f54f8c2ef0b05a5d28692288730332
didaclis <36189205+didaclis@users.noreply.github.com>
2020-01-22 04:16:03 +0100

    phone number

---
+++ AssemblyGame/Assets/Script/Actions/BottonPhoneAction.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottonPhoneAction : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Action()
+    {
+        int value;
+        // attempt to parse the value using the TryParse functionality of the integer type
+        int.TryParse(name, out value);
+        transform.parent.parent.GetComponent<PhoneAction>().SetNumber(value);
+    }
+}
---
+++ AssemblyGame/Assets/Script/Actions/PhoneAction.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PhoneAction : AbstractAction
+{
+    public Camera camera;
+    public GameObject dialogueManager;
+    public GameObject[] bottons;
+    public int[] correctNumber;
+    int N_insert_number = 0;
+    Vector3 on_position;
+
+    private Queue<int> inserted_number;
+   
+    void Start()
+    {
+        correctNumber = new int[9];
+        inserted_number = new Queue<int>();
+    }
+    public override void OnAction()
+    {
+        N_insert_number = 0;
+        camera.GetComponent<blur>().BlurIn();
+        on_position = transform.position;
+        StartCoroutine(SpriteCenter(camera));
+        for (int i = 0; i < 9; ++i)
+        {
+            bottons[i].SetActive(true);
+        }
+    } 
+    public override void EndAction()
+    {
+        while(inserted_number.Count < 9)
+        {
+           
+        }
+        camera.GetComponent<blur>().BlurOut();
+        StartCoroutine(ReturnOriginalPos(on_position));
+        StopAllCoroutines();
+        for (int i = 0; i < 9; ++i)
+        {
+            bottons[i].SetActive(false);
+        }
+    }
+
+    public void SetNumber(int num)
+    {
+        inserted_number.Enqueue(num);
+        Debug.Log(num);
+        ++N_insert_number;
+    }
+}
